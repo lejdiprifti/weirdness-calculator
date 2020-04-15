e0e52f5cf182d0f2bad78866e33d7fb6490137ed
lejdiprifti <priftilejdi@gmail.com>
2020-04-15 02:11:30 +0200

    created a model for liked gif

--- calculator/src/app/calculator/dashboard/dashboard.component.ts
+++ calculator/src/app/calculator/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GifService } from 'src/app/service/gif.service';
+import { LikedGif } from 'src/app/models/liked-gif';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,19 +12,21 @@ export class DashboardComponent implements OnInit {
   data: any;
   weirdness: number;
   phrase: string;
-  myLikes: Set<string>;
+  myLikes: Array<LikedGif>;
+  gif: LikedGif;
   constructor(private gifService: GifService) { }
 
   ngOnInit() {
     this.data = {}
-    this.myLikes = new Set()
+    this.myLikes = []
+    this.gif = {}
     this.weirdness = 0
   }
 
   search(phrase: string): void {
     this.phrase = phrase
     this.gifService.getByPhraseAndWeirdness(phrase, 0).subscribe(res => {
-      this.data = res.data.images.original.url
+      this.gif = this.createLikedGif(res)
     }, err=> {
       console.log(err)
     })
@@ -31,14 +34,27 @@ export class DashboardComponent implements OnInit {
 
   searchForWeirdness(): void {
     this.gifService.getByPhraseAndWeirdness(this.phrase, this.weirdness).subscribe(res => {
-      this.data = res.data.images.original.url
+      this.gif = this.createLikedGif(res)
     }, err => {
       console.log(err)
     })
   }
 
   like(): void {
-    this.myLikes.add(this.data)
+    if ( this.myLikes.filter(el => el.phrase === this.gif.phrase).length === 0 ) {
+      this.myLikes.push(this.gif)
+    } else {
+      console.log('Choose another one. The phrase is taken!')
+    }
+  }
+
+  createLikedGif(response: any): LikedGif {
+    return {
+      title: response.data.title,
+      phrase: this.phrase,
+      url: response.data.images.original.url,
+      weirdness: this.weirdness
+    }
   }
 
 }
---
+++ calculator/src/app/models/liked-gif.ts
@@ -0,0 +1,6 @@
+export interface LikedGif {
+    title?: string,
+    phrase?: string,
+    url?: string,
+    weirdness?: number
+}
